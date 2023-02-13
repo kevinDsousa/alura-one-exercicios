1e222f8ace197cec352bcc6ce166ac8e2445c93c
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-02-13 11:32:02 -0300

    Treinando geters e seters

--- backend_java/alura_bank/src/Conta.java
+++ backend_java/alura_bank/src/Conta.java
@@ -1,8 +1,8 @@
 public class Conta {
-    double saldo;
-    int agencia;
-    int numero;
-    Cliente titular;
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
 
     void depositar(double valor) {
         this.saldo += valor;
@@ -24,5 +24,25 @@ public class Conta {
             return true;
         }
         return false;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
     }
 }
