0419dca74e6b2296b1dd3bf50b3f58e7a4b2b16f
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-02-16 09:54:13 -0300

    Testando a classe funcionario e metodos

--- backend_java/heranca/src/App.java
+++ backend_java/heranca/src/App.java
@@ -1,7 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Funcionario nico = new Funcionario("nico silva", "05745749330");
+        Funcionario nico = new Funcionario();
         nico.setSalario((260.00));
         System.out.println(nico.getNome());
+
+        FuncionarioTest kevin = new FuncionarioTest();
+        kevin.setTipo(0);
+        kevin.setSalario(300.00);
+
+        FuncionarioTest diego = new FuncionarioTest();
+        diego.setTipo(1);
+        diego.setSalario(500.00);
+
+        System.out.println(kevin.getBonificacao());
+        System.out.println(diego.getBonificacao());
+        
     }
 }
--- backend_java/heranca/src/FuncionarioTest.java
+++ backend_java/heranca/src/FuncionarioTest.java
@@ -1,10 +1,28 @@
-public class Funcionario {
+public class FuncionarioTest {
     private String nome;
     private String cpf;
     private double salario;
+    private int tipo = 0;
+    private int senha;
 
     public double getBonificacao() {
-        return this.salario * 0.1;
+        if(this.tipo == 0) {
+            return this.salario * 0.1;
+        }
+        else if(this.tipo == 1) {
+            return this.salario + 500;
+        }
+        else {
+            return this.salario + 1000;
+        }
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getTipo() {
+        return tipo;
     }
 
     public void setNome(String nome) {
