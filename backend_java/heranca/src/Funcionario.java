9a00a9f891b13098a6949b2f622b589b10917b57
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-02-17 17:53:08 -0300

    usando classes herdadas e moldando metodos

--- backend_java/heranca/src/Funcionario.java
+++ backend_java/heranca/src/Funcionario.java
@@ -4,7 +4,7 @@ public class Funcionario {
     private double salario;
 
     public double getBonificacao() {
-        return this.salario * 0.1;
+        return this.salario * 0.05;
     }
 
     public void setNome(String nome) {
