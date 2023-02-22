0f9deb340f9d87ccabde0a291b521d6c898065b2
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-02-22 10:11:04 -0300

    Aprendendo mais sobre overide

--- backend_java/heranca_construtores/src/App.java
+++ backend_java/heranca_construtores/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ContaCorrente conta = new ContaCorrente(144, 200);
+        conta.depositar(100.0);
+
+        ContaPoupanca contaP = new ContaPoupanca(222, 333);
+        contaP.depositar(200.0);
+
+        conta.transfere(10, contaP);
+        conta.saca(50);
+
+        System.out.println(conta.getSaldo());
+        System.out.println(contaP.getSaldo());
     }
 }
--- backend_java/heranca_construtores/src/ContaCorrente.java
+++ backend_java/heranca_construtores/src/ContaCorrente.java
@@ -3,4 +3,10 @@ public class ContaCorrente extends Conta {
     public ContaCorrente(int agencia, int numero) {
         super(agencia, numero);
     }
+
+    @Override
+    public boolean saca(double valor) {
+        double valorASacar = valor + 0.2;
+        return super.saca(valorASacar);
+    }
 }
